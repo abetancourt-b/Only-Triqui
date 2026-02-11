8f9a45de1459d2d4ecda9348c2376bd4cb0dcf25
abetancourt-b <abetancourtt@unal.edu.co>
2026-02-11 10:35:39 -0500

    Update Only-Triqui.js

--- Only-Triqui.js
+++ Only-Triqui.js
@@ -11,25 +11,30 @@ function setup() {
 }
 
 function draw() {
-  background(50);
+  background(255);
   
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
       x = i * tamaño
       y = j * tamaño
       
-      if (matriz[i][j] === 0) {
-        fill(20)
-      } else {
-        fill(0, 0, 255)
-      }
-      
-      stroke(10)
+      stroke("black")
+      strokeWeight(4)
       rect(x, y, tamaño, tamaño)
       
-      noStroke()
-      fill(255, 100)
-      circle(x + 50, y + 50, 30)
+      if (matriz[i][j] === 0) {
+        fill(255)
+      } else if (matriz[i][j] === 1) {
+          stroke("blue")
+          line(x + 33, y + 33 , x + 66, y + 66)
+          line(x + 66, y + 33, x + 33, y + 66)
+      } else {
+          fill(255,0)
+          stroke("red")
+          circle(x + 50, y + 50, 45)
+      }
+      
+      
       
     }
   }
@@ -39,9 +44,9 @@ function mousePressed() {
   let i = floor(mouseX / tamaño)
   let j = floor(mouseY / tamaño)
   
-  if (matriz[i][j] === 0) {
-    matriz[i][j] = 1
+  if (matriz[i][j] === 1) {
+    matriz[i][j] = 2
   } else {
-    matriz[i][j] = 0
+    matriz[i][j] = 1
   }
 }
